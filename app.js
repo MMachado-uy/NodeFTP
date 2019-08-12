998a1162efeb6226e22b017611bd99508d12b735
Mauricio Machado <mauricio.machado@modelit.xyz>
2019-08-12 19:57:53 -0300

    FTP PoC

---
+++ app.js
@@ -0,0 +1,24 @@
+const ftp = require("basic-ftp");
+require("dotenv").config();
+console.log(process.env)
+example()
+ 
+async function example() {
+    const client = new ftp.Client()
+    client.ftp.verbose = true
+    try {
+        await client.access({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD,
+            port: process.env.DEFAULT_PORT
+        })
+        await client.cd(process.env.DEFAULT_FOLDER);
+
+        console.log(await client.list())
+    }
+    catch(err) {
+        console.log(err)
+    }
+    client.close()
+}
